b8c6758d23d88f7915e6d65e564c3bff61a97631
Michael Fouquet <michael.fouquet@me.com>
2017-06-04 18:06:12 -0500

    Finish up the site

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
+$(document).ready(function() {
+  $('#nav-home').click(function(e) {
+    $('.nav-item').removeClass('nav-item--active');
+    $('#nav-home').addClass('nav-item--active');
 
+    $('#home').addClass('u-show--block').removeClass('u-hide');
+    $('#installation').addClass('u-hide').removeClass('u-show');
+    $('#usage').addClass('u-hide').removeClass('u-show');
+  });
+
+  $('#nav-installation').click(function(e) {
+    $('.nav-item').removeClass('nav-item--active');
+    $('#nav-installation').addClass('nav-item--active');
+
+    $('#home').addClass('u-hide').removeClass('u-show--block');
+    $('#installation').addClass('u-show').removeClass('u-hide');
+    $('#usage').addClass('u-hide').removeClass('u-show');
+  });
+
+  $('#nav-usage').click(function(e) {
+    $('.nav-item').removeClass('nav-item--active');
+    $('#nav-usage').addClass('nav-item--active');
+
+    $('#home').addClass('u-hide').removeClass('u-show--block');
+    $('#installation').addClass('u-hide').removeClass('u-show');
+    $('#usage').addClass('u-show').removeClass('u-hide');
+  });
+
+  $('#download').click(function(e) {
+    $('.nav-item').removeClass('nav-item--active');
+    $('#nav-installation').addClass('nav-item--active');
+
+    $('#home').addClass('u-hide').removeClass('u-show--block');
+    $('#installation').addClass('u-show').removeClass('u-hide');
+    $('#usage').addClass('u-hide').removeClass('u-show');
+  });
+});
